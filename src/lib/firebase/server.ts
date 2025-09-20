8a12ed396bcc0dab8e3df1734d0a436c75fe40f6
MAISHA YA SUPERSTAR <elonjazz89@gmail.com>
2025-09-20 12:31:26 +0000

    Try fixing this error: `Runtime Error: TypeError: Cannot read properties

--- src/lib/firebase/server.ts
+++ src/lib/firebase/server.ts
@@ -1,11 +1,16 @@
 // src/lib/firebase/server.ts
 import * as admin from 'firebase-admin';
 
+const firebaseConfig = {
+  "projectId": "studio-3569606942-35878"
+};
+
 // Initialize Firebase Admin SDK
 if (!admin.apps.length) {
   admin.initializeApp({
     // Use application default credentials in a managed environment
     credential: admin.credential.applicationDefault(),
+    projectId: firebaseConfig.projectId,
   });
 }
 
