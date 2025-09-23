c4c624bdf75504e21c276219d6b19521f0be681a
MAISHA YA SUPERSTAR <elonjazz89@gmail.com>
2025-09-23 10:48:45 +0000

    Try fixing this error: `Runtime Error: Error: Can't find meta/_journal.j

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,10 +1,6 @@
 import { drizzle } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
 import * as schema from './schema';
-import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
 
 const sqlite = new Database('sqlite.db');
 export const db = drizzle(sqlite, { schema });
-
-// Run migrations
-migrate(db, { migrationsFolder: 'drizzle' });
